76a4e2cd594882503e248a14e1acb02125bfa35a
MaxBooth <maxebooth@icloud.com>
2020-10-06 18:31:06 +0100

    tutorial
    tutorial

--- student_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class student_pageConfig(AppConfig):
-    name = 'student_page'
--- tutor_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TutorPageConfig(AppConfig):
-    name = 'tutor_page'
